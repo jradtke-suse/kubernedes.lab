2edb89b8b50e3d975c74912ec879cb201ffa7475
James Radtke - SUSE <james.radtke@suse.com>
2025-06-27 15:44:36 -0400

    generic update

---
+++ Scripts/build_haproxy.sh
@@ -0,0 +1,25 @@
+
+
+
+zypper in haproxy keepalived
+
+echo "net.ipv4.ip_nonlocal_bind = 1" | sudo tee -a /etc/sysctl.d/20_keepalive.conf
+
+cat << EOF | sudo tee /etc/keepalived/keepalived.conf
+vrrp_instance VI_1 {
+    state MASTER           # On backup servers, set to BACKUP
+    interface eth0         # Change to your network interface
+    virtual_router_id 100  # Must be unique per VIP
+    priority 101           # Higher on master, lower on backup
+    authentication {
+        auth_type PASS
+        auth_pass yourpassword
+    }
+    virtual_ipaddress {
+        10.10.12.120/22   # Your VIP and subnet
+    }
+}
+EOF
+
+sudo systemctl enable keepalived
+
