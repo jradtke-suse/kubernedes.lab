daab351fa67329c13a9fdfd21136b9a2e4d822fc
James Radtke <cloudxabide@gmail.com>
2025-06-27 16:17:30 -0400

    generic update

--- Scripts/build_haproxy.sh
+++ Scripts/build_haproxy.sh
@@ -1,6 +1,6 @@
 
 
-
+# using Keepalived for floating/VIP (and to future proof)
 zypper in haproxy keepalived
 
 echo "net.ipv4.ip_nonlocal_bind = 1" | sudo tee -a /etc/sysctl.d/20_keepalive.conf
@@ -22,4 +22,5 @@ vrrp_instance VI_1 {
 EOF
 
 sudo systemctl enable keepalived
-
+cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.$(uuidgen | tr -d '-' | head -c 6)"
+curl -O /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/jradtke-suse/rancher.kubernerdes.lab/refs/heads/main/Files/etc_haproxy_haproxy.cfg
